f32621fac8ed1d6180a51ea7cc31ad434a1f6f41
masa-suzu <szk.masato@icloud.com>
2018-10-28 19:28:25 +0900

    Add syntax

--- README.md
+++ README.md
@@ -3,10 +3,36 @@ monkey interpreter in TypeScript
 
 Inspired by https://github.com/masa-suzu/monkey.
 
+monkey interpreter implements a repl.
 ```
-monkey > Hello Monkey!
-Token { Type: 'IDENT', Literal: 'Hello' }
-Token { Type: 'IDENT', Literal: 'Monkey' }
-Token { Type: '!', Literal: '!' }
-Token { Type: 'EOF', Literal: '' }
+monkey > let hello = monkey
+let hello = monkey;
 ```
+
+## Syntax
+As of now, these syntaxes are available.
+* literal
+  * integer: "1" -> 1
+  * true: "true" -> true
+  * false: "false" -> false
+  * function: "fn(x) {return x}" -> fn(x) return x;
+
+* statement
+  * let: "let x = 1" -> let x = 1;
+  * return: "return x" -> return x;
+
+* expression
+  * if: "if(x) {return x}" -> if x return x;
+  * if-else: "if(x) {return x} else{return 0}" -> if x return x; else return 0;
+  * call: "add(1,2)" -> add(1, 2)
+
+* operator
+  * !: "!x" -> (!x)
+  * +: "x + y" -> (x + y)
+  * -: "x - y" -> (x - y)
+  * *: "x * y" -> (x * y)
+  * /: "x / y" -> (x / y)
+  * ==: "x == y" -> (x == y)
+  * !=: "x != y" -> (x != y)
+  * >: "x > y" -> (x > y)
+  * <: "x < y" -> (x < y)
---
+++ test/syntax.test.ts
@@ -0,0 +1,108 @@
+import * as chai from "chai"
+import * as lexer from "../src/lexer/lexer"
+import * as parser from "../src/parser/parser"
+
+describe('literal', () => {
+    interface test {
+        name: string
+        input: string
+        want: string
+    }
+    let tests: test[] = [
+        { name:"integer",input: "1", want: "1", },
+        { name:"true",input: "true", want: "true", },
+        { name:"false",input: "false", want: "false", },
+        { name:"function",input: "fn(x) {return x}", want: "fn(x) return x;", },
+    ];
+    tests.forEach(tt => {
+        it(`${tt.name}: \"${tt.input}\" -> ${tt.want}`, () => {
+            let l = new lexer.Lexer(tt.input);
+            let p = parser.Parser.New(l);
+            let program = p.ToProgram();
+            chai.expect(p.Errors().length).equal(0, Helper.ToString(p.Errors()));
+            chai.expect(program.Statements[0].Node().String()).equal(tt.want);
+        });
+    });
+})
+
+describe('statement', () => {
+    interface test {
+        name: string
+        input: string
+        want: string
+    }
+    let tests: test[] = [
+        { name:"let",input: "let x = 1", want: "let x = 1;", },
+        { name:"return",input: "return x", want: "return x;", },
+    ];
+    tests.forEach(tt => {
+        it(`${tt.name}: \"${tt.input}\" -> ${tt.want}`, () => {
+            let l = new lexer.Lexer(tt.input);
+            let p = parser.Parser.New(l);
+            let program = p.ToProgram();
+            chai.expect(p.Errors().length).equal(0, Helper.ToString(p.Errors()));
+            chai.expect(program.Statements[0].Node().String()).equal(tt.want);
+        });
+    });
+})
+
+describe('expression', () => {
+    interface test {
+        name: string
+        input: string
+        want: string
+    }
+    let tests: test[] = [
+        { name:"if",input: "if(x) {return x}", want: "if x return x;", },
+        { name:"if-else",input: "if(x) {return x} else{return 0}", want: "if x return x; else return 0;", },
+        { name:"call",input: "add(1,2)", want: "add(1, 2)", },
+    ];
+    tests.forEach(tt => {
+        it(`${tt.name}: \"${tt.input}\" -> ${tt.want}`, () => {
+            let l = new lexer.Lexer(tt.input);
+            let p = parser.Parser.New(l);
+            let program = p.ToProgram();
+            chai.expect(p.Errors().length).equal(0, Helper.ToString(p.Errors()));
+            chai.expect(program.Statements[0].Node().String()).equal(tt.want);
+        });
+    });
+})
+
+describe('operator', () => {
+    interface test {
+        name: string
+        input: string
+        want: string
+    }
+    let tests: test[] = [
+        { name:"!",input: "!x", want: "(!x)", },
+        { name:"+",input: "x + y", want: "(x + y)", },
+        { name:"-",input: "x - y", want: "(x - y)", },
+        { name:"*",input: "x * y", want: "(x * y)", },
+        { name:"/",input: "x / y", want: "(x / y)", },
+        { name:"==",input: "x == y", want: "(x == y)", },
+        { name:"!=",input: "x != y", want: "(x != y)", },
+        { name:">",input: "x > y", want: "(x > y)", },
+        { name:"<",input: "x < y", want: "(x < y)", },
+    ];
+    tests.forEach(tt => {
+        it(`${tt.name}: \"${tt.input}\" -> ${tt.want}`, () => {
+            let l = new lexer.Lexer(tt.input);
+            let p = parser.Parser.New(l);
+            let program = p.ToProgram();
+            chai.expect(p.Errors().length).equal(0, Helper.ToString(p.Errors()));
+            chai.expect(program.Statements[0].Node().String()).equal(tt.want);
+        });
+    });
+})
+
+class Helper {
+    static ToString(values: string[]): string {
+        let s = "";
+
+        values.forEach(element => {
+            s += ("\t" + element + "\n");
+        });
+        return s;
+    }
+}
