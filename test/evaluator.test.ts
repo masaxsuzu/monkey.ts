89e46a891d3ca5daedf7b54d5a68f731c7bf0318
masa-suzu <szk.masato@icloud.com>
2018-10-29 00:56:32 +0900

    refactoring
    add )
    
    refactoring

--- test/evaluator.test.ts
+++ test/evaluator.test.ts
@@ -4,46 +4,78 @@ import * as parser from "../src/parser/parser"
 import * as object from "../src/object/object"
 import * as evaluator from "../src/evaluator/evaluator"
 
-describe('Object', () => {
-    interface test {
-        input: string
-        want: {value:any,literal:string}
-    }
+interface test {
+    input: string
+    want: { value: any, literal: string }
+}
+
+describe('IntegerObject', () => {
+
     let tests: test[] = [
-        { input: "1", want: {value:1,literal:"1"}, },
-        { input: "2", want: {value:2,literal:"2"}, },
-        { input: "true", want: {value:true,literal:"true"}, },
-        { input: "false", want: {value:false,literal:"false"}, },
-        { input: "!true", want: {value:false,literal:"false"}, },
-        { input: "!false", want: {value:true,literal:"true"}, },
-        { input: "!5", want: {value:false,literal:"false"}, },
-        { input: "!!5", want: {value:true,literal:"true"}, },
-        { input: "-5", want: {value:-5,literal:"-5"}, },
-        { input: "-15", want: {value:-15,literal:"-15"}, },
+        { input: "1", want: { value: 1, literal: "1" }, },
+        { input: "-5", want: { value: -5, literal: "-5" }, },
+        { input: "-15", want: { value: -15, literal: "-15" }, },
+        
     ];
     tests.forEach(tt => {
+        let got = <object.Integer>Eval(tt.input);
         it(`${tt.input} -> ${tt.want.value}`, () => {
-            let got = <object.Integer>Helper.Eval(tt.input);
-
-            chai.expect(got.Value,"Value").equal(tt.want.value);
-        });
+            chai.expect(got.Value, "Value").equal(tt.want.value);
+        })
     });
 })
 
-class Helper{
-    public static Eval(input:string):Object{
-        let l = new lexer.Lexer(input);
-            let p = parser.Parser.New(l);
-            let program = p.ToProgram();
-            chai.expect(p.Errors().length).equal(0, Helper.ToString(p.Errors()));
-            return evaluator.Evaluate(program);
-    }
-    static ToString(values: string[]): string {
-        let s = "";
+describe('BoolObject', () => {
 
-        values.forEach(element => {
-            s += ("\t" + element + "\n");
-        });
-        return s;
-    }
+    let tests: test[] = [
+        { input: "true", want: { value: true, literal: "true" }, },
+        { input: "false", want: { value: false, literal: "false" }, },
+        { input: "!5", want: { value: false, literal: "false" }, },
+        { input: "!!5", want: { value: true, literal: "true" }, },
+        // { input: "1<2", want: { value: true, literal: "false" }, },
+        // { input: "1>2", want: { value: false, literal: "false" }, },
+        // { input: "1<1", want: { value: false, literal: "false" }, },
+        // { input: "1>1", want: { value: false, literal: "false" }, },
+        // { input: "1==1", want: { value: true, literal: "false" }, },
+        // { input: "1!=1", want: { value: false, literal: "false" }, },
+        // { input: "1==2", want: { value: false, literal: "false" }, },
+        // { input: "1!=2", want: { value: true, literal: "false" }, },
+    ];
+    tests.forEach(tt => {
+        let got = <object.Bool>Eval(tt.input);
+        it(`${tt.input} -> ${tt.want.value}`, () => {
+            chai.expect(got.Value, "Value").equal(tt.want.value);
+        })
+    });
+})
+
+describe('NullObject', () => {
+
+    let tests: test[] = [
+        { input: "-true", want: { value: object.Type.NULL_OBJ, literal: "null" }, },
+    ];
+    tests.forEach(tt => {
+        let got = <object.Null>Eval(tt.input);
+        it(`${tt.input} -> ${tt.want.value}`, () => {
+            chai.expect(got.Inspect()).equal(tt.want.literal);
+            chai.expect(got.Type()).equal(tt.want.value);
+        })
+    });
+})
+
+
+function Eval(input: string): Object {
+    let l = new lexer.Lexer(input);
+    let p = parser.Parser.New(l);
+    let program = p.ToProgram();
+    chai.expect(p.Errors().length).equal(0, ToString(p.Errors()));
+    return evaluator.Evaluate(program);
+}
+function ToString(values: string[]): string {
+    let s = "";
+
+    values.forEach(element => {
+        s += ("\t" + element + "\n");
+    });
+    return s;
 }
