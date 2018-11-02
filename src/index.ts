807cafcb2b56d8ecbb85c9f7408be3c52d4254f8
masa-suzu <szk.masato@icloud.com>
2018-11-02 19:33:53 +0900

    add html and js for github pages

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Lexer } from "../src/lexer/lexer"
+import { Parser } from './parser/parser';
+import { Evaluate } from './evaluator/evaluator';
+import { NewEnvironment } from './object/environment';
+
+var env = NewEnvironment();
+var src,run,out;
+
+export function Run(input: string): string {
+    let l = new Lexer(input);
+    let p = Parser.New(l);
+    let program = p.ToProgram();
+    if (p.Errors().length != 0) {
+        return printParseErrors(p.Errors());
+    }
+
+    let got = Evaluate(program, env);
+    if (got == null) { return "" }
+
+    return got.Inspect();
+}
+
+function printParseErrors(errors: string[]): string {
+    let out = "";
+    errors.forEach(e => {
+        out += `${e}\n`;
+    });
+
+    return out;
+}
+onload = function () {
+    src = document.getElementById('text1');
+    out = document.getElementById('text2');
+    run = document.getElementById('run');
+    src.onkeyup  = function () {
+        let got = Run(src.innerText);
+        out.innerText = got; 
+    }
+}
